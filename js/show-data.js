bc6d448893ba45209fb58ab81c102c6a4b698043
riordi80 <riordi80@gmail.com>
2024-11-10 19:57:12 +0000

    7 commit

--- js/show-data.js
+++ js/show-data.js
@@ -1,17 +1,17 @@
 //Function to show the data stored in LocalStorage
-function showData(){
-    const TITLE = localStorage.getItem("title"); //Meto dentro de la constante NAME el valor de "name"
-    document.getElementById("title").innerHTML = TITLE;
- 
-    const AUTOR = localStorage.getItem("autor");
-    document.getElementById("autor").innerHTML = AUTOR;
- 
-    const CATEGORY = localStorage.getItem("category");
-    document.getElementById("category").innerHTML = CATEGORY;
+function showData() {
+  const TITLE = localStorage.getItem("title"); //Meto dentro de la constante NAME el valor de "name"
+  document.getElementById("title").innerHTML = TITLE;
 
-    const CONDITION = localStorage.getItem("condition");
-    document.getElementById("condition").innerHTML = CONDITION;
-  }
- 
+  const AUTOR = localStorage.getItem("autor");
+  document.getElementById("autor").innerHTML = AUTOR;
+
+  const CATEGORY = localStorage.getItem("category");
+  document.getElementById("category").innerHTML = CATEGORY;
+
+  const CONDITION = localStorage.getItem("condition");
+  document.getElementById("condition").innerHTML = CONDITION;
+}
+
 //Call the function that shows the data stored in LocalStorage
-  showData();
+showData();
